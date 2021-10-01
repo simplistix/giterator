2befe1243dd8417b35979dcf6108271dedb51472
Chris Withers <chris@simplistix.co.uk>
2021-10-01 18:18:26 +0100

    Prepare for 0.2.0 release.

--- docs/changes.rst
+++ docs/changes.rst
@@ -3,6 +3,11 @@
 Changes
 =======
 
+0.2.0 (1 Oct 2021)
+------------------
+
+- Methods that create commits now return the newly-create commit hash.
+
 0.1.0 (28 Sep 2021)
 -------------------
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ base_dir = os.path.dirname(__file__)
 
 setup(
     name='giterator',
-    version='0.1.0',
+    version='0.2.0',
     author='Chris Withers',
     author_email='chris@withers.org',
     license='MIT',
